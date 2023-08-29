f94c4e9cd5dcd18ddddbeedddd7845c25da4bb6a
OctavioGarello <103843676+OctavioGarello@users.noreply.github.com>
2023-08-29 13:06:49 -0300

    -

--- Datos y Algoritmo/material viejo/Unidad 4/Ejercicio 4/main.py
+++
@@ -1,8 +0,0 @@
-from Huffman import Huffman
-from os import path
-
-if __name__ == '__main__':
-    Huffman().comprimir(
-		path.dirname(__file__) + '/input.txt',
-		path.dirname(__file__) + '/output.txt'
-	)
