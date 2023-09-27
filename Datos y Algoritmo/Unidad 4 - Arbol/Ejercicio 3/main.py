762a9e33ca6808b92280a7e71d53294b2938e4fb
OctavioGarello <103843676+OctavioGarello@users.noreply.github.com>
2023-09-27 02:03:28 -0300

    -

---
+++ Datos y Algoritmo/Unidad 4 - Arbol/Ejercicio 3/main.py
@@ -0,0 +1,19 @@
+from arbolBB import ArbolBinarioBusqueda
+import os
+
+if __name__ == "__main__":
+    os.system("cls")
+    abb= ArbolBinarioBusqueda()
+    abb.insertar(5)
+    abb.insertar(3)
+    abb.insertar(2)
+    abb.insertar(4)
+    abb.insertar(7)
+    abb.insertar(6)
+    abb.insertar(8)
+    abb.mostrarArbolBin()
+    
+    frontera=[]
+
+    abb.frontera(frontera)
+    print("\nLa frontera del arbol es: ", frontera)
